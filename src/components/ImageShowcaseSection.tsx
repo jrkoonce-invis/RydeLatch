5d711c4d330dfa69e9cc1cf24da75fc4629947cc
Jameson Koonce <jamesonkoonce@gmail.com>
2025-10-20 21:43:54 -0500

    image fix

--- src/components/ImageShowcaseSection.tsx
+++ src/components/ImageShowcaseSection.tsx
@@ -37,7 +37,7 @@ const ProductTechSection = () => {
           <div className="rounded-2xl sm:rounded-3xl overflow-hidden shadow-elegant animate-on-scroll">
             <div className="w-full">
               <img 
-                src="public/newassets/Ryde Latch Scene Photo 5.png" 
+                src="public/newassets/Ryde Latch Scene Photo 5.jpg" 
                 alt="Rydelatch assembly and installation" 
                 className="w-full h-auto object-cover"
               />
